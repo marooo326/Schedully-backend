6233c38bcd5515d8e34b03e77afc96e6554d0112
marooo326 <ktnqkr01@gmail.com>
2023-06-17 13:47:09 +0900

    initial commit

---
+++ src/main/java/schedully/schedully/SchedullyApplication.java
@@ -0,0 +1,13 @@
+package schedully.schedully;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedullyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedullyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/schedully/schedully/SchedullyApplicationTests.java
@@ -0,0 +1,13 @@
+package schedully.schedully;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedullyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
