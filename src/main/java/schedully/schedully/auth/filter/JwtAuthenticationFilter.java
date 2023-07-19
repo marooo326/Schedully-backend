f764d4d9445d6917fff1cc70c13b49b5a49eadb4
marooo326 <ktnqkr01@gmail.com>
2023-07-19 17:36:27 +0900

    feat: JWT 토큰 Filter 및 Provider 구현

---
+++ src/main/java/schedully/schedully/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,43 @@
+package schedully.schedully.auth.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.GenericFilterBean;
+import schedully.schedully.auth.provider.JwtTokenProvider;
+
+import java.io.IOException;
+@Slf4j
+public class JwtAuthenticationFilter extends GenericFilterBean {
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = resolveToken((HttpServletRequest) request);
+
+        // 토큰 유효성 검사
+        if (token!=null && jwtTokenProvider.validateToken(token)) {
+            Authentication authentication = jwtTokenProvider.getAuthentication(token);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        log.trace("JwtAuthenticationFilter's doFilter : "+token);
+        chain.doFilter(request, response);
+    }
+
+    // 헤더에서 토큰 추출
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
--- src/main/java/schedully/schedully/config/SecurityConfig.java
+++ src/main/java/schedully/schedully/config/SecurityConfig.java
@@ -1,59 +1,59 @@
 package schedully.schedully.config;
 
-import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import schedully.schedully.auth.filter.JwtAuthenticationFilter;
+import schedully.schedully.auth.provider.JwtTokenProvider;
 
 
 @Configuration
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(AbstractHttpConfigurer::disable) // 비활성화
-                .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize   // lambda 방식
-                        .requestMatchers("/users/**").permitAll()
-                        .requestMatchers("/login").permitAll()
-                        //.dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
-                        .anyRequest().authenticated()    // 어떠한 요청이라도 인증필요
-                )
-                .formLogin(AbstractHttpConfigurer::disable)    // 대시보드 이동이 막히면 안되므로 얘는 허용
-                .logout(withDefaults())    // 로그아웃은 기본설정으로 (/logout으로 인증해제)
-                .httpBasic(Customizer.withDefaults())
-                .build();
-    }
 
-    /**
-     * PasswordEncoder를 Bean으로 등록
-     */
+    private final JwtTokenProvider jwtTokenProvider;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring()
+                .requestMatchers(
+                        "/api/member/**"
+//                        "/health",
+//                        "/",
+//                        "/swagger-ui.html",
+//                        "/v3/api-docs",
+//                        "/v3/api-docs/**",
+//                        "/swagger-ui/**",
+//                        "/docs/**"
+                        );
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable) // 비활성화
+                .cors(AbstractHttpConfigurer::disable)
+                .sessionManagement(manage-> manage.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Session 사용 안함
+                .formLogin(AbstractHttpConfigurer::disable)     // form login 사용 안함
+                .httpBasic(AbstractHttpConfigurer::disable)     // http basic 방식 사용 안함
+                .authorizeHttpRequests(authorize -> authorize   // lambda 방식
+                        .requestMatchers("/api/member").permitAll()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
                 .build();
-        return new InMemoryUserDetailsManager(user);
     }
 }
